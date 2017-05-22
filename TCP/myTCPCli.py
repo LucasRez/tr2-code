28b6318dba6ebef021c4004dfed5b71d8b59bad5
Lucas Rezende <rez.rez18@gmail.com>
2017-05-22 18:32:16 -0300

    whatever

--- TCP/myTCPCli.py
+++ TCP/myTCPCli.py
@@ -1,15 +1,26 @@
 from socket import *
-serverName = ''
+def myConnect(addr):
+	ack = ""
+	while (ack != "SYNACK"):
+		clientSocket.sendto("SYN", addr)
+		try:
+			ack, addr = clientSocket.recvfrom(2048)
+		except timeout:
+			print "Connection timed out, retrying"
+	clientSocket.sendto("ACK", addr)
+
+serverName = '' 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.sendto("SYN",(serverName, serverPort))
-response, serverAddress = clientSocket.recvfrom(2048)
-if (response != "ACK"):
-    print "Failed to establish connection, exiting..."
-    clientSocket.close()
-    exit()
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+timeoutVal = 3.0
+clientSocket.settimeout(timeoutVal)
+
+myConnect((serverName, serverPort))
+clientSocket.settimeout(None)
+
+message = raw_input('Input lowercase sentence:') 
+
+clientSocket.sendto(message,(serverName, serverPort)) 
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048) 
 print modifiedMessage
 clientSocket.close()
